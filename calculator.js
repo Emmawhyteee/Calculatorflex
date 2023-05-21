d103015b224868c3089bef36037b000608350c30
Nwankwo Emmanuel <emmatexiii@gmail.com>
2023-05-21 23:19:44 +0100

    code_updates

---
+++ calculator.js
@@ -0,0 +1,17 @@
+let btns = document.querySelectorAll(".num-button");
+let allBtns = document.querySelectorAll(".button");
+let resultBox = document.querySelectorAll("#result-box");
+let clearBtn = document.querySelectorAll("#clear");
+let totalBtn = document.querySelectorAll("#total");
+
+let btnSpread = [...btns];
+let allBtnSpread = [...allBtns];
+// for number inputs
+
+btnSpread.forEach((button, i) => {
+    button.addEventListener("click", () => {
+        //inner values for calculator
+        
+        
+    }
+}
