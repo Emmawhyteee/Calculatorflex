915e617a5d3a26780270fd5652250c6dbbe516e2
Nwankwo Emmanuel <emmatexiii@gmail.com>
2023-05-23 14:16:34 +0100

    new update

--- calculator.js
+++ calculator.js
@@ -9,9 +9,34 @@ let allBtnSpread = [...allBtns];
 // for number inputs
 
 btnSpread.forEach((button, i) => {
-    button.addEventListener("click", () => {
-        //inner values for calculator
-        
-        
+  button.addEventListener("click", () => {
+    //inner values for calculator
+
+    if (resultBox.innerHTML == "0") {
+      resultBox.innerHTML = "";
     }
+
+    let value = btns[i].innerHTML;
+    resultBox.innerHTML += value;
+  });
+});
+
+//function to evaluate strings below
+
+function evaluate(fn) {
+  return new Function("return " + fn)();
 }
+
+//to calculate all functions
+
+total.addEventListener("click", () => {
+  let allInputs = resultBox.innerHTML;
+  resultBox.innerHTML = evaluate(allInputs);
+  console.log(evaluate(allInputs));
+});
+
+//clear all inputs
+
+clearBtn.addEventListener("click", () => {
+  resultBox.innerHTML = "0";
+});
